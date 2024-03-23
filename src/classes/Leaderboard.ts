3a0237a131e4d1b6004887b820cfc4f251db6612
Noopel <30575718+Noopel@users.noreply.github.com>
2024-03-23 16:58:12 +0100

    Fixed

--- src/classes/Leaderboard.ts
+++ src/classes/Leaderboard.ts
@@ -72,7 +72,7 @@ class Leaderboard {
       seasonBtn.addEventListener("click", () => this.changeSeason(season));
     });
 
-    for (const [key, value] of Object.entries(this.totalMemberList)) {
+    for (const [, value] of Object.entries(this.totalMemberList)) {
       this.allMemberList.push(value);
       this.allMemberList.sort((a, b) => b.points - a.points);
     }
