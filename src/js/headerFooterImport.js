c53b6b458c3efc8d5f8059b52ca06cee4ef80dcb
Noopel <30575718+Noopel@users.noreply.github.com>
2024-03-23 19:04:38 +0100

    Update headerFooterImport.js

--- src/js/headerFooterImport.js
+++ src/js/headerFooterImport.js
@@ -3,5 +3,5 @@ import jQuery from "jquery"
 const headerContainer = document.querySelector("#headerContainer")
 
 if(headerContainer) {
-    jQuery.$(headerContainer).load("/html/header.html")
+    $(headerContainer).load("/html/header.html")
 }
